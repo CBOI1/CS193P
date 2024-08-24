82f6279766189be3db09cc916b6a471aa3a5e4d3
Christian <christian9720@berkeley.edu>
2024-08-24 12:40:14 -0700

    Finished Lecture 2

--- Memorize/ContentView.swift
+++ Memorize/ContentView.swift
@@ -8,31 +8,70 @@
 import SwiftUI
 
 struct ContentView: View {
+    let emojis = ["👾", "🤖", "👻", "🤡", "🦭"]
+    @State var cardCount = 2
     var body: some View {
-        HStack {
-            CardView(isFaceUp: true)
-            CardView(isFaceUp: false)
+        VStack {
+            ScrollView {
+                cards
+            }
+            Spacer()
+            cardAdjuster
+        }
+        .padding()
+    }
+    var cards : some View {
+        LazyVGrid(columns: [GridItem(), GridItem()]){
+            ForEach(0..<cardCount, id: \.self) {
+                CardView(content: emojis[$0], isFaceUp: true)
+                    .aspectRatio(2/3, contentMode: .fit)
+            }
         }
         .foregroundColor(.orange)
     }
+    func cardAdjuster(by offset : Int, symbol : String) -> some View {
+        Button {
+            cardCount += offset
+        } label: {
+            Image(systemName: symbol)
+        }
+        .disabled(cardCount + offset < 1 || cardCount + offset > emojis.count)
+    }
+    
+    
+    
+    var cardAdjuster : some View {
+        HStack {
+            cardAdjuster(by: -1, symbol: "minus.circle")
+            Spacer()
+            cardAdjuster(by: +1, symbol: "plus.circle")
+        }
+        .imageScale(.large)
+        .font(.largeTitle)
+    }
 }
+
 
 struct CardView : View {
     //Some comment
-    var isFaceUp : Bool
+    
+    let content : String
+    @State var isFaceUp : Bool = false
     var body : some View {
         ZStack {
-            if isFaceUp {
-                RoundedRectangle(cornerRadius: 12)
-                    .foregroundColor(.white)
-                RoundedRectangle(cornerRadius: 12).strokeBorder(lineWidth: 10)
-                Text("😀").font(.largeTitle)
-            } else {
-                RoundedRectangle(cornerRadius: 12)
+            let base =  RoundedRectangle(cornerRadius: 12)
+            Group {
+                base.fill(.white)
+                base.strokeBorder(lineWidth: 10)
+                Text(content).font(.largeTitle)
             }
+            .opacity(isFaceUp ? 1 : 0)
+            base.opacity(isFaceUp ? 0 : 1)
+        }
+        .onTapGesture {
+            isFaceUp.toggle()
         }
         
-        .padding()
     }
 }
 
@@ -64,8 +103,6 @@ struct CardView : View {
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         Group {
-            ContentView()
-                .previewInterfaceOrientation(.portraitUpsideDown)
             ContentView()
                 .previewInterfaceOrientation(.portraitUpsideDown)
         }
